4786cbd7986cb98b40b30bf90a745442ffa58603
Shivam Prajapati <sp488830@gmail.com>
2025-06-21 16:43:07 +0530

    Completed responisveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,54 +6,59 @@ const Footer = () => {
     /* deep‑blue backdrop, full‑width */
     <section className="w-full bg-[#002240]">
       {/* layout wrapper */}
-      <div className="mx-auto flex max-w-7xl items-center justify-between gap-10 px-6 py-16 sm:px-10 lg:px-10">
+      <div className="mx-auto flex max-w-7xl flex-col items-center justify-between gap-8 px-4 py-12 sm:px-6 sm:py-16 md:flex-row md:items-start md:gap-10 lg:px-10 xl:items-center">
         {/* ---------- LEFT : HEADLINE ---------- */}
-        <div className="relative max-w-2xl">
-          <h1 className="text-4xl sm:text-5xl lg:text-7xl font-extrabold leading-[1.08] tracking-tight uppercase">
+        <div className="relative w-full max-w-2xl text-center md:text-left">
+          <h1 className="text-3xl font-extrabold leading-[1.1] tracking-tight uppercase sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl xl:leading-[1.08]">
             {/* line 1 */}
             <span className="block text-white">
-              Ready&nbsp;to&nbsp;boost&nbsp;your
+              Ready&nbsp;to&nbsp;
+              <span className="text-[#caa8ff]">boost</span>
+              &nbsp;your
             </span>
 
             {/* line 2 with overlay icon */}
-            <span className="block relative text-[#caa8ff]">
-              Brand’s&nbsp;Strategic
-              {/* overlay comment/like bubble */}
+            <span className="relative flex flex-col items-center justify-center text-white sm:flex-row sm:justify-start md:flex-row">
+              <span className="block">
+                Brand's&nbsp;
+                <span className="text-[#caa8ff] pl-15">Strategic</span>
+              </span>
+              {/* overlay comment/like bubble - responsive positioning */}
               <Image
                 src="/Comment.png"
                 alt="Like notification"
-                width={70}
-                height={70}
+                width={80}
+                height={80}
                 priority
-                className="absolute -top-8 left-1/2 -translate-x-1/2 rotate-6 drop-shadow-lg"
+                className="absolute -top-6 right-4 rotate-6 drop-shadow-lg sm:-top-8 sm:right-8 sm:h-[100px] sm:w-[100px] md:-top-10 md:left-1/2 md:right-auto md:-translate-x-1/2 lg:-top-12 lg:h-[120px] lg:w-[120px]"
               />
             </span>
 
             {/* line 3 */}
             <span className="block">
               <span className="text-[#caa8ff]">Impact</span>
-              &nbsp;and&nbsp;
+              &nbsp;<span className="text-[#caa8ff]">And</span>&nbsp;
               <span className="text-white">Achieve</span>
             </span>
 
             {/* line 4 */}
             <span className="block text-[#caa8ff]">
-              Significant&nbsp;Growth<span className="text-white">?</span>
+              <span className="text-white">Significant</span>&nbsp;Growth
+              <span className="text-white">?</span>
             </span>
           </h1>
         </div>
 
-        {/* ---------- RIGHT : ORANGE SQUARE ---------- */}
+        {/* ---------- RIGHT : FILE ICON ---------- */}
         <div className="shrink-0">
-          <div className="">
-            <Image
-              src="/FileIcon.png" // arrow graphic
-              alt="Up‑right arrow"
-              width={320}
-              height={320}
-              priority
-            />
-          </div>
+          <Image
+            src="/FileIcon.png"
+            alt="File icon graphic"
+            width={200}
+            height={200}
+            priority
+            className="h-auto w-[200px] sm:w-[240px] md:w-[280px] lg:w-[320px]"
+          />
         </div>
       </div>
     </section>
